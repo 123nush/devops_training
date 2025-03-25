7e788b56ae918085839b2e79e456b9ad81571692
Anushka Dalvi <119521757+123nush@users.noreply.github.com>
2025-03-25 15:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-print("Welcom to Deloitte")
-print("Devops Training")
-print("This is from main brach")
-print("This is from Dev1")
+from flask import Flask 
+import os 
+app = Flask(__name__) 
+@app.route('/') 
+
+def hello(): 
+    return ('\nWelcome to Deloitte Guys! \n\n')
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=8080, debug=True)
